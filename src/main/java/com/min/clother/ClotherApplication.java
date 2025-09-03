a6ff5f5bea23b19f011999fc18942eade20a1373
mmmv41 <tlsalsrud5068@daum.net>
2025-09-03 16:20:29 +0900

    📦️Chore: 초기 프로젝트 세팅

---
+++ src/main/java/com/min/clother/ClotherApplication.java
@@ -0,0 +1,13 @@
+package com.min.clother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClotherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClotherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/min/clother/ClotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min.clother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClotherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
